030411b7c8e2ab187cda4f93689be186f41022ed
beodata <office@beodata.rs>
2021-04-12 11:32:14 +0200

    moved from ini to config

--- app/Core/DB.php
+++ app/Core/DB.php
@@ -9,37 +9,39 @@ class DB
     private $sQuery;
     private $settings;
     private $bConnected = false;
-   // private $log;
+// private $log;
     private $parameters;
-
-
     public function __construct()
     {
         $this->Connect();
         $this->parameters = array();
     }
 
-
     private function connect()
     {
-        $this->settings = parse_ini_file("settings.ini.php");
-        $dsn            = 'mysql:dbname=' . $this->settings["dbname"] . ';host=' . $this->settings["host"] . '';
+        $dsn = DB_DRIVER . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=UTF8';
+
+      //  $this->settings = parse_ini_file("./app/config/config.php");
+      //  $dsn = 'mysql:dbname=' . $this->settings["dbname"] . ';host=' . $this->settings["host"] . '';
+
+
         try {
-            # podešavanja iz INI datoteke, set UTF8
-            $this->pdo = new \PDO($dsn, $this->settings["user"], $this->settings["password"], array(
-                \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-            ));
+            $this->pdo = new \PDO($dsn, DB_USERNAME, DB_PASSWORD);
 
-            # evidet. bilo kakve izuzetke u slučaju fatalne greške.
-            $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
-            # Disable emulation
-            $this->pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
 
-            # Veza je uspela,
-            $this->bConnected = true;
+        # evidet. bilo kakve izuzetke u slučaju fatalne greške.
+                $this
+                    ->pdo
+                    ->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        # Disable emulation
+                $this
+                    ->pdo
+                    ->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+        # Veza je uspela,
+                $this->bConnected = true;
         } catch (\PDOException $e) {
-            # Write into log
+        # Write into log
             echo $this->ExceptionLog($e->getMessage());
             die();
         }
@@ -51,7 +53,6 @@ class DB
         $this->pdo = null;
     }
 
-
     private function init($query, $parameters = "")
     {
 
@@ -59,49 +60,49 @@ class DB
             $this->Connect();
         }
         try {
-            $this->sQuery = $this->pdo->prepare($query);
-
+            $this->sQuery = $this
+                ->pdo
+                ->prepare($query);
             $this->bindMore($parameters);
-
-
             if (!empty($this->parameters)) {
                 foreach ($this->parameters as $param => $value) {
                     if (is_int($value[1])) {
-                        $type = \PDO::PARAM_INT;
-                    } else if (is_bool($value[1])) {
-                        $type = \PDO::PARAM_BOOL;
-                    } else if (is_null($value[1])) {
+                            $type = \PDO::PARAM_INT;
+                    } elseif (is_bool($value[1])) {
+                                $type = \PDO::PARAM_BOOL;
+                    } elseif (is_null($value[1])) {
                         $type = \PDO::PARAM_NULL;
                     } else {
                         $type = \PDO::PARAM_STR;
                     }
 
-                    $this->sQuery->bindValue($value[0], $value[1], $type);
+                    $this
+                        ->sQuery
+                        ->bindValue($value[0], $value[1], $type);
                 }
             }
 
-
-            $this->sQuery->execute();
+            $this
+                ->sQuery
+                ->execute();
         } catch (\PDOException $e) {
             echo $this->ExceptionLog($e->getMessage(), $query);
             die();
         }
 
-
         $this->parameters = array();
     }
 
-
     public function bind($para, $value)
     {
-        $this->parameters[sizeof($this->parameters)] = [":" . $para, $value];
+        $this->parameters[sizeof($this->parameters) ] = [":" . $para, $value];
     }
 
     public function bindMore($parray)
     {
         if (empty($this->parameters) && is_array($parray)) {
             $columns = array_keys($parray);
-            foreach ($columns as $i => &$column) {
+            foreach ($columns as $i => & $column) {
                 $this->bind($column, $parray[$column]);
             }
         }
@@ -110,55 +111,58 @@ class DB
     public function query($query, $params = null, $fetchmode = \PDO::FETCH_ASSOC)
     {
         $query = trim(str_replace("\r", " ", $query));
-
         $this->Init($query, $params);
-
         $rawStatement = explode(" ", preg_replace("/\s+|\t+|\n+/", " ", $query));
-
-        # Which SQL statement is used
+# Which SQL statement is used
         $statement = strtolower($rawStatement[0]);
-
         if ($statement === 'select' || $statement === 'show') {
-            return $this->sQuery->fetchAll($fetchmode);
+            return $this
+                ->sQuery
+                ->fetchAll($fetchmode);
         } elseif ($statement === 'insert' || $statement === 'update' || $statement === 'delete') {
-            return $this->sQuery->rowCount();
+            return $this
+                ->sQuery
+                ->rowCount();
         } else {
             return null;
         }
     }
 
-
     public function lastInsertId()
     {
-        return $this->pdo->lastInsertId();
+        return $this
+            ->pdo
+            ->lastInsertId();
     }
-
 
     public function beginTransaction()
     {
-        return $this->pdo->beginTransaction();
+        return $this
+            ->pdo
+            ->beginTransaction();
     }
-
 
     public function executeTransaction()
     {
-        return $this->pdo->commit();
+        return $this
+            ->pdo
+            ->commit();
     }
-
 
     public function rollBack()
     {
-        return $this->pdo->rollBack();
+        return $this
+            ->pdo
+            ->rollBack();
     }
-
 
     public function column($query, $params = null)
     {
         $this->Init($query, $params);
-        $Columns = $this->sQuery->fetchAll(\PDO::FETCH_NUM);
-
+        $Columns = $this
+            ->sQuery
+            ->fetchAll(\PDO::FETCH_NUM);
         $column = null;
-
         foreach ($Columns as $cells) {
             $column[] = $cells[0];
         }
@@ -169,16 +173,26 @@ class DB
     public function row($query, $params = null, $fetchmode = \PDO::FETCH_ASSOC)
     {
         $this->Init($query, $params);
-        $result = $this->sQuery->fetch($fetchmode);
-        $this->sQuery->closeCursor(); // Oslobadja vezu
+        $result = $this
+            ->sQuery
+            ->fetch($fetchmode);
+        $this
+            ->sQuery
+            ->closeCursor();
+// Oslobadja vezu
         return $result;
     }
 
     public function single($query, $params = null)
     {
         $this->Init($query, $params);
-        $result = $this->sQuery->fetchColumn();
-        $this->sQuery->closeCursor(); //  Oslobadjaa vezu
+        $result = $this
+            ->sQuery
+            ->fetchColumn();
+        $this
+            ->sQuery
+            ->closeCursor();
+//  Oslobadjaa vezu
         return $result;
     }
 
@@ -187,8 +201,6 @@ class DB
         $exception = '  Unhandled Exception. <br />';
         $exception .= $message;
         $exception .= $sql;
-
-
         if (!empty($sql)) {
             $message .= "\r\nRaw SQL : " . $sql;
         }
@@ -200,7 +212,6 @@ class DB
     public function __clone()
     {
     }
-
 
     public function __wakeup()
     {
--- app/config/config.php
+++ app/config/config.php
@@ -1,11 +1,11 @@
 <?php
 
-define('BASE_PATH', 'http://localhost/faq2');
+define('BASE_PATH', 'http://localhost/222');
 define('BASE_ASSET_PATH', 'public');
 
 define('DB_DRIVER', 'mysql');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '123');
 define('DB_NAME', 'faq');
 define('DB_HOST', 'localhost');
 
